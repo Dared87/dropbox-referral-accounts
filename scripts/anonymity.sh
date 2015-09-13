f8373b0062764f07ea1b80880e906a1993e5089d
Dorian Villet <gnutix@gmail.com>
2015-09-13 16:22:50 +0200

    Fix TOR installation because of a down keyserver

--- scripts/anonymity.sh
+++ scripts/anonymity.sh
@@ -3,7 +3,7 @@
 echo "Install TOR"
 sudo add-apt-repository "deb http://deb.torproject.org/torproject.org trusty main"
 
-gpg --keyserver keys.gnupg.net --recv 886DDD89
+gpg --keyserver keyserver.ubuntu.com --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
 sudo apt-get update
